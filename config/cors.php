1786291720ae8b46c4c8233ca7ba6d6579f9c50b
Diego Vargas <diego@soho.cl>
2020-09-11 23:07:02 -0500

    add origin cors all

--- config/cors.php
+++ config/cors.php
@@ -34,7 +34,7 @@ return [
     /*
      * Matches the request origin. `[*]` allows all origins. Wildcards can be used, eg `*.mydomain.com`
      */
-    'allowed_origins' => ['http://localhost:8080'],
+    'allowed_origins' => ['*'],
 
     /*
      * Patterns that can be used with `preg_match` to match the origin.
